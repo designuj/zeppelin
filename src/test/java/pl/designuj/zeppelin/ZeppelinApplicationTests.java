e33c108703374bca21c2f8b2f465669e7d1c9be3
tom <designuj@fejm.pl>
2021-01-03 14:52:19 +0000

    first commit

---
+++ src/test/java/pl/designuj/zeppelin/ZeppelinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.designuj.zeppelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeppelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
